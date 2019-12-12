2a492160675a031db0a7ef16b00c766ee3880dd2
Ignazio Merendino <merens92@gmail.com>
2019-12-12 15:22:46 +0100

    add characterization test

--- src/test/java/BottlesTest.java
+++ src/test/java/BottlesTest.java
@@ -8,16 +8,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class BottlesTest {
-//    @Test
-//    public void testSong() throws IOException {
-//        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//        Bottles.writeLyrics(
-//                new PrintStream(
-//                        stream));
-//
-//        final String actual = new String(stream.toByteArray());
-//        final String expected = new String(Files.readAllBytes(Paths.get("src/main/resources/test.txt")));
-//
-//        Assert.assertEquals(expected,actual);
-//    }
+    @Test
+    public void testSong() throws IOException {
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        Bottles.writeLyrics(
+                new PrintStream(
+                        stream));
+
+        final String actual = new String(stream.toByteArray());
+        final String expected = new String(Files.readAllBytes(Paths.get("src/main/resources/test.txt")));
+
+        Assert.assertEquals(expected,actual);
+    }
 }
